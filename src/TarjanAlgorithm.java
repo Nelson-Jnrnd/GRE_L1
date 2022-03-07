e7335d6c26eb119adde26d0d95f9bcce0fba900c
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2022-03-07 16:22:34 +0100

    refactorisation

--- src/TarjanAlgorithm.java
+++ src/TarjanAlgorithm.java
@@ -4,24 +4,35 @@ import java.io.IOException;
 import java.util.Stack;
 
 public class TarjanAlgorithm {
-    private final DirectedGraph graph;
-    private final int[] scc;
-    private final int[] dfsnum;
-    private final int[] low;
+    private DirectedGraph graph;
+    private int[] scc;
+    private int[] dfsnum;
+    private int[] low;
     private int dfsnumCounter;
-    private int conectedComponentCounter;
+    private int connectedComponentCounter;
     private Stack<Integer> visitedVertices;
 
+    public TarjanAlgorithm() {}
 
-    public TarjanAlgorithm(DirectedGraph graph) {
+    private void init(DirectedGraph graph){
         this.graph = graph;
         this.scc = new int[graph.getNVertices()];
         this.dfsnum = new int[scc.length];
         this.low = new int[scc.length];
-        visitedVertices = new Stack<>();
+        this.visitedVertices = new Stack<>();
+    }
+    public int nbConnectedComponents(DirectedGraph graph){
+        sccTarjan(graph);
+        return connectedComponentCounter;
     }
 
-    public int[] sccTarjan() {
+    public int[] conectedComponents(DirectedGraph graph){
+        return sccTarjan(graph).clone();
+    }
+
+    private int[] sccTarjan(DirectedGraph graph) {
+        init(graph);
+
         // For each vertex in graph
         for (int vertex = 0; vertex < scc.length; vertex++) { // TODO potentiellement intutile -> array init at 0
             scc[vertex] = 0;
@@ -29,14 +40,14 @@ public class TarjanAlgorithm {
         }
 
         dfsnumCounter = 0;
-        conectedComponentCounter = 0;
+        connectedComponentCounter = 0;
         visitedVertices.empty();
 
         for (int vertex = 0; vertex < scc.length; vertex++) {
             if(scc[vertex] == 0)
                 scc(vertex);
         }
-        return scc.clone();
+        return scc;
     }
 
     private void scc(Integer vertex) {
@@ -51,10 +62,10 @@ public class TarjanAlgorithm {
                 low[vertex] = Math.min(low[vertex], low[successor]);
         }
         if(low[vertex] == dfsnum[vertex]){ // pop until we get to vertex
-            conectedComponentCounter++;
+            connectedComponentCounter++;
             while(true){ // TODO maybe faut pas désempiller quand pop = vertex
                 Integer removedVertex = visitedVertices.pop();
-                scc[removedVertex] = conectedComponentCounter;
+                scc[removedVertex] = connectedComponentCounter;
                 if(removedVertex.equals(vertex)){
                     break;
                 }
@@ -83,14 +94,15 @@ public class TarjanAlgorithm {
                     graph = DirectedGraphReader.fromFile(fileEntry.getPath());
 
                     System.out.println(graph + " in file " + fileEntry.getName());
-                    TarjanAlgorithm tarj = new TarjanAlgorithm(graph);
-                    int[] composants = tarj.sccTarjan();
+                    TarjanAlgorithm tarj = new TarjanAlgorithm();
 
-                    int vertex = 0;
+                    System.out.println("Number of components : " + tarj.nbConnectedComponents(graph));
+                    //int[] composants = tarj.sccTarjan(graph);
+
+                    /*int vertex = 0;
                     for (int composant: composants) {
                         System.out.println("Vertex " + vertex++ + " in composant -> " + composant);
-                    }
-                    break; // TODO remove
+                    }*/
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
