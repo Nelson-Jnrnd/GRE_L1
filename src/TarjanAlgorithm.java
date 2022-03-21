a36c4c2fb5cc491e350aed232a77cba041e75efb
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2022-03-21 17:23:12 +0100

    finished comments

--- src/TarjanAlgorithm.java
+++ src/TarjanAlgorithm.java
@@ -3,17 +3,44 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Stack;
 
+/**
+ * Object that calculates the connected components of a given connected graph using the tarjan algorithm
+ * @author Nelson Jeanrenaud
+ * @version 1.0
+ */
 public class TarjanAlgorithm {
+    /**
+     * Graph to process
+     */
     private DirectedGraph graph;
+    /**
+     * Successor array
+     */
     private int[] scc;
+    /**
+     * Order in the search of each vertex (index)
+     */
     private int[] dfsnum;
+    /**
+     * Oldest ancester we can reach from each vertex (index)
+     */
     private int[] low;
+    /**
+     * Counter of the search
+     */
     private int dfsnumCounter;
+    /**
+     * Number of connected components found
+     */
     private int connectedComponentCounter;
     private Stack<Integer> visitedVertices;
 
     public TarjanAlgorithm() {}
 
+    /**
+     * Initialize the variables for a particular graph
+     * @param graph graph on which to apply the tarjan algorithm
+     */
     private void init(DirectedGraph graph){
         this.graph = graph;
         this.scc = new int[graph.getNVertices()];
@@ -21,23 +48,33 @@ public class TarjanAlgorithm {
         this.low = new int[scc.length];
         this.visitedVertices = new Stack<>();
     }
+
+    /**
+     * Calculate the number of connected components for a given directed graph
+     * @param graph the input graph on which we count the connected components
+     * @return Number of connected components
+     */
     public int nbConnectedComponents(DirectedGraph graph){
         sccTarjan(graph);
         return connectedComponentCounter;
     }
 
+    /**
+     * Returns the connected components of a given directed graph
+     * @param graph the input graph on which we calculate the connected components
+     * @return array containning the number of the component each vertex (index) is part of.
+     */
     public int[] conectedComponents(DirectedGraph graph){
         return sccTarjan(graph).clone();
     }
 
+    /**
+     * Apply the tarjan algorithm on a directed graph
+     * @param graph given graph on which we apply the algorithm
+     * @return array containning the number of the component each vertex (index) is part of.
+     */
     private int[] sccTarjan(DirectedGraph graph) {
         init(graph);
-
-        // For each vertex in graph
-        for (int vertex = 0; vertex < scc.length; vertex++) { // TODO potentiellement intutile -> array init at 0
-            scc[vertex] = 0;
-            dfsnum[vertex] = 0;
-        }
 
         dfsnumCounter = 0;
         connectedComponentCounter = 0;
@@ -50,20 +87,25 @@ public class TarjanAlgorithm {
         return scc;
     }
 
+    /**
+     * Depth search to find all the other vertices in the same connected component
+     * @param vertex vertex we start the depth search from
+     */
     private void scc(Integer vertex) {
         dfsnum[vertex] = ++dfsnumCounter;
         low[vertex] = dfsnumCounter;
 
         visitedVertices.push(vertex);
         for (int successor: graph.getSuccessorList(vertex)) {
-            if(dfsnum[successor] == 0) // Not yet visited
+            if(dfsnum[successor] == 0) // is vertex not yet visited
                 scc(successor);
-            if(scc[successor] == 0)
+            if(scc[successor] == 0) // does vertex allow us to go further up the tree
                 low[vertex] = Math.min(low[vertex], low[successor]);
         }
         if(low[vertex] == dfsnum[vertex]){ // pop until we get to vertex
             connectedComponentCounter++;
-            while(true){ // TODO maybe faut pas désempiller quand pop = vertex
+            // mark all the vertices of the tree with the connected component number
+            while(true){
                 Integer removedVertex = visitedVertices.pop();
                 scc[removedVertex] = connectedComponentCounter;
                 if(removedVertex.equals(vertex)){
@@ -82,27 +124,22 @@ public class TarjanAlgorithm {
 
         DirectedGraph graph;
 
+        // A filter that will only accept files ending with `.txt`
         File[] files = folder.listFiles(new FilenameFilter() {
             public boolean accept(File directory, String fileName) {
                 return fileName.endsWith(".txt");
             }
         });
+        // A way to avoid null pointer exception.
         if(files != null){
+            TarjanAlgorithm tarj = new TarjanAlgorithm();
             for (final File fileEntry : files) {
                 try {
-
                     graph = DirectedGraphReader.fromFile(fileEntry.getPath());
 
                     System.out.println(graph + " in file " + fileEntry.getName());
-                    TarjanAlgorithm tarj = new TarjanAlgorithm();
 
                     System.out.println("Number of components : " + tarj.nbConnectedComponents(graph));
-                    //int[] composants = tarj.sccTarjan(graph);
-
-                    /*int vertex = 0;
-                    for (int composant: composants) {
-                        System.out.println("Vertex " + vertex++ + " in composant -> " + composant);
-                    }*/
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
